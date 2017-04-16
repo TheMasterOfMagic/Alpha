10e70de8d7f97d8e033617eb413bf9602fe24dc6
钟总 <18811569079@163.com>
2017-04-16 23:36:33 +0800

    【超级汉诺塔】完成√

--- Hanoi/Hanoi/test.cpp
+++ Hanoi/Hanoi/test.cpp
@@ -1,25 +1,65 @@
+#include <math.h>
+#include <stack>
 #include <iostream>
 using namespace std;
 
-void move(int from, int to) {
-	cout << from << " -> " << to << endl;
+typedef stack<int> Column;
+Column columns[3];
+Column invert(Column c) {
+	Column temp;
+	while (!c.empty()) {
+		temp.push(c.top());
+		c.pop();
+	}
+	return temp;
+}
+void f(int id, int n,int&plate, int& from, int& to) {
+	int T = id - (id&(id - 1));
+	int k = id / T / 2;
+	plate = (int)log2(T)+1;
+	from = (plate + n) % 2 ? k % 3 + 1 : 3 - (k + 2) % 3;
+	to = (plate + n) % 2 ? (k + 1) % 3 + 1 : 3 - k % 3;
+}
+void move(Column& from,Column& to) {
+	to.push(from.top());
+	from.pop();
+}
+void showColumns() {
+	Column temp;
+	for (int i = 0; i < 3; ++i) {
+		temp = invert(columns[i]);
+		cout << "柱" << i + 1 << ":";
+		while (!temp.empty()) {
+			cout << temp.top() << ' ';
+			temp.pop();
+		}
+		cout << endl;
+	}
 }
 void Hanoi(int mount) {
 	const int totalSteps = pow(2, mount) - 1;
-	int id, from, to,times;
-	for (int i = 1; i <= totalSteps; ++i) {
-		id = i;
-		for (times = 1; (id / times + 1) % 2; times *= 2);
-		cout << id << '\t' << times << endl;
+	int plate,from, to;
+	showColumns();
+	for (int id = 1; id <= totalSteps; ++id) {
+		f(id, mount, plate,from, to);
+		cout << "第" << id << "步，将盘" << plate << "从柱" << from << "移到柱" << to << endl;
+		move(columns[from-1], columns[to-1]);
+		showColumns();
 	}
 }
 int main() {
-	int n=3;
-	
-	
+	Column& column1 = columns[0];
+	Column& column2 = columns[1];
+	Column& column3 = columns[2];
+	int n;
 	while (1) {
+		column1 = Column();
+		column2 = Column();
+		column3 = Column();
 		cin >> n;
+		for (int i = n; i >= 1; --i) column1.push(i);
 		system("cls");
 		Hanoi(n);
+		
 	}
 }
