4e813d549290e2c94fcc70191652fc5f99c9fb3a
钟总 <18811569079@163.com>
2018-10-22 10:38:36 +0800

    添加了第二次实验中计算Follow集与Select集的相关代码

--- CompilingTheory/exp2/Grammar.py
+++ CompilingTheory/exp2/Grammar.py
@@ -21,13 +21,11 @@ class Grammar:
 			for right_part in right_part_set:
 				print('\t{} → {}'.format(left_part, right_part or epsilon))
 
-	@debug(2)
+	@debug(0)
 	def get_first(self, alpha: str):
-		log('当前正在计算{}的First集'.format(alpha))
 		if alpha in self.firsts:
-			log('\t已经计算过，直接返回')
-			log()
 			return self.firsts[alpha]
+		log('当前正在计算{}的First集'.format(alpha))
 		self.firsts[alpha] = set()
 		rv = set()
 		log('{} 有以下右部: {}'.format(alpha, '(' + ', '.join(map(lambda x: x or epsilon, self.table[alpha])) + ')'))
@@ -78,3 +76,118 @@ class Grammar:
 	def get_firsts(self):
 		for vn in self.table.keys():
 			self.get_first(vn)
+
+	@debug(0)
+	def get_follow(self, alpha: str):
+		if alpha in self.follows:
+			return self.follows[alpha]
+		log('当前正在计算{}的Follow集'.format(alpha))
+		self.follows[alpha] = set()
+		rv = set()
+		for left_part, right_part_set in self.table.items():
+			for right_part in right_part_set:
+				if alpha in right_part:
+					log('\t在产生式{} → {}中发现了{}'.format(left_part, right_part, alpha))
+					i = right_part.index(alpha)
+					while True:
+						c = right_part[i+1:i+2]
+						log('\t其后面一个字符是{}'.format(c or epsilon))
+						if c.islower():
+							log('\t\t是非空终结符')
+							log('\t\t直接将其加入结果中')
+							rv.add(c)
+							break
+						elif c == '':
+							log('\t\t是空串')
+							log('\t\t故将Follow({})并入结果中'.format(left_part))
+							if left_part not in self.follows:
+								log('\t\t而Follow({})尚未计算'.format(left_part))
+								log('\t\t计算之')
+								follow = self.get_follow(left_part)
+							else:
+								follow = self.follows[left_part]
+								if not follow:
+									log('\t\t而Follow({})正在计算中'.format(left_part))
+									log('\t\t忽略')
+							if follow:
+								log('\t\tFollow({})={}，将其并入结果'.format(left_part, follow))
+								rv = rv.union(follow)
+							break
+						elif c.isupper():
+							log('\t\t是非终结符')
+							first = self.get_first(c)
+							log('\t\tFirst({})={}'.format(c, first))
+							if '' not in first:
+								log('\t\t其中没有空串')
+								log('\t\t故直接将其并入结果中')
+								rv = rv.union(first)
+								break
+							else:
+								log('\t\t其中包含空串')
+								log('\t\t所以将除空串以外的内容并入结果中')
+								log('\t\t再对{} → {}中{}的下一个字符进行分析'.format(left_part, right_part, c))
+								rv = rv.union(first.difference({''}))
+								i += 1
+						else:
+							raise Exception
+		if alpha == self.start:
+			log('由于{}是开始符，故将#加入到其Follow集中'.format(alpha))
+			rv.add('#')
+		log('计算得Follow({})={}'.format(alpha, rv))
+		self.follows[alpha] = rv
+		log()
+		return rv
+
+	def get_follows(self):
+		for vn in self.table.keys():
+			self.get_follow(vn)
+
+	@debug(0)
+	def get_select(self, left_part: str, right_part: str):
+		log('当前正在计算{} → {}的Select集'.format(left_part, right_part or epsilon))
+		log('在此之前先计算First({})'.format(right_part or epsilon))
+		i = 0
+		first = set()
+		while True:
+			c = right_part[i:i+1]
+			log('\t当前字符: {}'.format(c or epsilon))
+			if c.islower() or c == '':
+				log('\t\t是终结符')
+				log('\t\t故直接将其加入到结果中')
+				first.add(c)
+				break
+			elif c.isupper():
+				log('\t\t是非终结符')
+				first_c = self.firsts[c]
+				log('\t\t而First({})={}'.format(c, first_c))
+				if '' not in first_c:
+					log('\t\t其中不包含空串')
+					log('\t\t故将其并入结果中')
+					first = first.union(first_c)
+					break
+				else:
+					log('\t\t其中包含空串')
+					log('\t\t所以将除空串以外的内容并入结果中')
+					log('\t\t再对{} → {}中{}的下一个字符进行分析'.format(left_part, right_part, c))
+					first = first.union(first_c.difference({''}))
+					i += 1
+			else:
+				raise Exception
+		log('计算得First({})={}'.format(right_part, first))
+		if '' not in first:
+			log('其中不包含空串')
+			rv = first
+			log('故Select({} → {})=First({})={}'.format(left_part, right_part, right_part, first))
+		else:
+			log('其中包含空串')
+			rv = first.difference({''}).union(self.follows[left_part])
+			log('故Select({} → {})=First({})-{}+Follow({})={}'.format(
+				left_part, right_part or epsilon, right_part or epsilon, '{' + epsilon + '}', left_part, rv))
+		self.selects[(left_part, right_part)] = rv
+		log()
+		return rv
+
+	def get_selects(self):
+		for left_part, right_part_set in self.table.items():
+			for right_part in right_part_set:
+				self.get_select(left_part, right_part)
--- CompilingTheory/exp2/main.py
+++ CompilingTheory/exp2/main.py
@@ -1,4 +1,5 @@
 from Grammar import Grammar
+from CompilingTheory.utils import epsilon
 
 
 def main():
@@ -10,9 +11,22 @@ def main():
 		M={'K', 'bLM'}
 	)
 	g = Grammar(gt, 'S')
+
 	g.get_firsts()
 	for k, v in sorted(g.firsts.items()):
 		print(k, v)
+	print()
+
+	g.get_follows()
+	for k, v in sorted(g.follows.items()):
+		print(k, v)
+	print()
+
+	g.get_selects()
+	for k, v in sorted(g.selects.items()):
+		if v:
+			left, right = k
+			print('{} → {}'.format(left, right or epsilon), v)
 
 
 if __name__ == '__main__':
